af3fd728009514d60a19eddecb405b186cc428ab
Husman <usmanoladapo69@gmail.com>
2022-09-15 13:04:23 -1100

    updated

--- explore_page.py
+++
@@ -1,9 +0,0 @@
-
-
-# Bar plot of Crop_Damage_value_counts
-df.groupby(['Outcome'])['Glucose'].mean().plot(
-    kind = 'bar',
-    xlabel = 'Diabetes Class', 
-    ylabel = 'Glucose Average count',
-    title = 'Glucose level of Patient Diabetes status'
-);
